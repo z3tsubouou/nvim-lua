a6c4f8a02a7a0bb4950d99eaab88bb9dd9fc339b
Davgatseren <25279427+z3tsubouou@users.noreply.github.com>
2022-08-11 23:33:56 +0800

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+uses packer manager
---
+++ after/plugin/material.rc.lua
@@ -0,0 +1,9 @@
+local status, material = pcall(require, "material")
+if (not status) then return end
+
+material.setup({
+})
+
+vim.g.material_style = "oceanic"
+
+vim.cmd 'colorscheme material'
