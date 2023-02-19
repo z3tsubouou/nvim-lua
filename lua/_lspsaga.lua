b990860bea2fb89be4bd25292e8eca7d1306bda3
Davgatseren <25279427+z3tsubouou@users.noreply.github.com>
2023-02-19 17:07:23 +0800

    todo nemsen

--- lua/_lspsaga.lua
+++ lua/_lspsaga.lua
@@ -19,6 +19,7 @@ local opts = {
     silent = true
 }
 vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
+-- Todo: hover hiisen uyed noice notification garch irj baigaag boliulalh
 vim.keymap.set('n', 'gk', '<Cmd>Lspsaga hover_doc<CR>', opts)
 vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 -- vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
