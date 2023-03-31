7f25273fe860ad55b525650eb8bfd44d7eac00fd
Davgatseren <25279427+z3tsubouou@users.noreply.github.com>
2023-03-31 23:35:07 +0800

    Feature: gob = go back to the prev cursor

--- lua/_lspsaga.lua
+++ lua/_lspsaga.lua
@@ -18,6 +18,11 @@ local opts = {
   noremap = true,
   silent = true
 }
+local opts2 = {
+  noremap = true,
+  silent = true,
+  remap = true
+}
 vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 -- Todo: hover hiisen uyed noice notification garch irj baigaag boliulalh
 vim.keymap.set('n', 'gk', '<Cmd>Lspsaga hover_doc<CR>', opts)
@@ -25,6 +30,7 @@ vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 vim.keymap.set('n', 'gca', '<Cmd>Lspsaga code_action<CR>', opts)
 vim.keymap.set('n', 'gtt', '<Cmd>Lspsaga term_toggle<CR>', opts)
 vim.keymap.set('n', 'gol', '<Cmd>Lspsaga outline<CR>', opts)
+vim.keymap.set("n", "gob", "<C-O>", opts2)
 -- vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
 -- vim.keymap.set('n', 'gp', '<Cmd>Lspsaga preview_definition<CR>', opts)
 -- vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
