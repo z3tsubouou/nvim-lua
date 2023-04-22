5b0215f4d3f7912148caaeff80a79c71fd0623e4
Davgatseren <25279427+z3tsubouou@users.noreply.github.com>
2023-04-22 13:20:55 +0800

    Fix

--- lua/_lspconfig.lua
+++ lua/_lspconfig.lua
@@ -144,3 +144,17 @@ require 'lspconfig'.lua_ls.setup {
     },
   },
 }
+
+require('lspconfig').yamlls.setup {
+  on_attach = on_attach,
+  settings = {
+    yaml = {
+      schemas = {
+        ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+        ["../path/relative/to/file.yml"] = "/.github/workflows/*",
+        ["/path/from/root/of/project"] = "/.github/workflows/*",
+        ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "/*.k8s.yaml",
+      },
+    },
+  }
+}
