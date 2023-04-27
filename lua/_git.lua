34dcf249aece02c682c99cb2d0c4ddeaa47f913e
Davgatseren <25279427+z3tsubouou@users.noreply.github.com>
2023-04-27 16:29:32 +0800

    changed git map

--- lua/_git.lua
+++ lua/_git.lua
@@ -11,19 +11,19 @@ git.setup({
     -- Open blame commit
     blame_commit = "<CR>",
     -- Open file/folder in git repository
-    browse = "<Leader>go",
+    -- browse = "<Leader>go",
     -- Open pull request of the current branch
-    open_pull_request = "<Leader>gp",
+    -- open_pull_request = "<Leader>gp",
     -- Create a pull request with the target branch is set in the `target_branch` option
-    create_pull_request = "<Leader>gn",
+    -- create_pull_request = "<Leader>gn",
     -- Opens a new diff that compares against the current index
     diff = "<Leader>gd",
     -- Close git diff
     diff_close = "<Leader>gD",
     -- Revert to the specific commit
-    revert = "<Leader>gr",
+    -- revert = "<Leader>gr",
     -- Revert the current file to the specific commit
-    revert_file = "<Leader>gR",
+    -- revert_file = "<Leader>gR",
   },
   -- -- Default target branch when create a pull request
   -- target_branch = "master",
