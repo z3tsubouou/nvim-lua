fe29414053ebf81125efc1243b2002fa085d575d
Davgatseren <25279427+z3tsubouou@users.noreply.github.com>
2023-12-19 17:43:18 +0800

    removed telescope picker height and added codeium

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -94,7 +94,7 @@ return {
             grouped = true,
             previewer = false,
             initial_mode = "normal",
-            layout_config = { height = 40 },
+            -- layout_config = { height = 40 },
           })
         end,
         desc = "Open File Browser with the path of the current buffer",
@@ -292,6 +292,25 @@ return {
         },
         sorting = defaults.sorting,
       }
+    end,
+  },
+  {
+    "Exafunction/codeium.vim",
+    event = "BufEnter",
+    config = function()
+      -- Change '<C-g>' here to any keycode you like.
+      vim.keymap.set("i", "<C-g>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true, silent = true })
+      vim.keymap.set("i", "<c-h>", function()
+        return vim.fn["codeium#CycleCompletions"](1)
+      end, { expr = true, silent = true })
+      vim.keymap.set("i", "<c-l>", function()
+        return vim.fn["codeium#CycleCompletions"](-1)
+      end, { expr = true, silent = true })
+      vim.keymap.set("i", "<c-x>", function()
+        return vim.fn["codeium#Clear"]()
+      end, { expr = true, silent = true })
     end,
   },
 }
