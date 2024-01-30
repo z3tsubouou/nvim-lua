571bc376e912aa7d75cf62d778fb206f17ba5a05
Davgatseren <25279427+z3tsubouou@users.noreply.github.com>
2024-01-30 15:07:45 +0800

    colorscheme uurchlult

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,24 +1,67 @@
 return {
   -- add gruvbox
-  { "rebelot/kanagawa.nvim" },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = true,
+    priority = 1000,
+    config = function()
+      require("kanagawa").setup({
+        transparent = true,
+      })
+    end,
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+      require("gruvbox").setup({
+        transparent_mode = true,
+      })
+    end,
+  },
+  {
+    "AlexvZyl/nordic.nvim",
+    lazy = true,
+    priority = 1000,
+    config = function()
+      require("nordic").load({
+        transparent_bg = true,
+      })
+    end,
+  },
+  {
+    "craftzdog/solarized-osaka.nvim",
+    branch = "osaka",
+    lazy = true,
+    priority = 1000,
+    opts = function()
+      return {
+        transparent = true,
+      }
+    end,
+  },
 
-  -- {
-  --   "craftzdog/solarized-osaka.nvim",
-  --   branch = "osaka",
-  --   lazy = true,
-  --   priority = 1000,
-  --   opts = function()
-  --     return {
-  --       transparent = false,
-  --     }
-  --   end,
-  -- },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    lazy = true,
+    config = {
+      require("catppuccin").setup({
+        background = {
+          light = "frappe",
+          dark = "mocha",
+        },
+        transparent_background = true,
+      }),
+    },
+  },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "catppuccin",
     },
   },
 }
