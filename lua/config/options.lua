d06a2362757b14edee65bd65395feabd6222394e
Davgatseren <25279427+z3tsubouou@users.noreply.github.com>
2024-06-03 11:46:45 +0800

    cursor line column nemsen

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,7 +11,8 @@ vim.opt.encoding = "utf-8"
 vim.opt.fileencoding = "utf-8"
 
 vim.opt.number = true
-
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
 vim.opt.title = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
