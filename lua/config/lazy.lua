88563d7ece66372450fbcaa58457a4600bf17f00
Davgatseren <25279427+z3tsubouou@users.noreply.github.com>
2024-07-01 18:20:44 +0800

    plugin extra nemsen saijruulsan

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,6 +24,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.toml" },
+    { import = "lazyvim.plugins.extras.lang.svelte" },
+
+    { import = "lazyvim.plugins.extras.formatting.black" },
 
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
