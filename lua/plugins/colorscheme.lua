2582c234a27d9d394675cc19dbb9355cc8870558
Davgatseren <25279427+z3tsubouou@users.noreply.github.com>
2024-07-09 15:42:44 +0800

    theme system theme hamaardag bolgoson

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,7 +5,28 @@ return {
     lazy = true,
     priority = 1000,
     opts = {
-      -- transparent = true,
+      compile = false, -- enable compiling the colorscheme
+      undercurl = true, -- enable undercurls
+      commentStyle = { italic = true },
+      functionStyle = {},
+      keywordStyle = { italic = true },
+      statementStyle = { bold = true },
+      typeStyle = {},
+      transparent = false, -- do not set background color
+      dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+      terminalColors = true, -- define vim.g.terminal_color_{0,17}
+      colors = { -- add/modify theme and palette colors
+        palette = {},
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+      },
+      overrides = function(colors) -- add/modify highlights
+        return {}
+      end,
+      theme = "wave", -- Load "wave" theme when 'background' option is not set
+      background = { -- map the value of 'background' option to a theme
+        dark = "wave", -- try "dragon" !
+        light = "dragon",
+      },
     },
   },
   {
@@ -46,7 +67,7 @@ return {
       transparent_background = true,
     },
   },
-
+  { "EdenEast/nightfox.nvim" }, -- lazy
   {
     "folke/twilight.nvim",
     opts = {
