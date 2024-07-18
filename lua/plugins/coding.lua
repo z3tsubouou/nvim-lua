58e88a4eba2b29bba924d2871aee8a30648f4297
Davgatseren <25279427+z3tsubouou@users.noreply.github.com>
2024-07-18 14:43:05 +0800

    refactor editor config and added coding lua file

---
+++ lua/plugins/coding.lua
@@ -0,0 +1,202 @@
+return {
+  -- Create annotations with one keybind, and jump your cursor in the inserted annotation
+  {
+    "danymat/neogen",
+    keys = {
+      {
+        "<leader>cg",
+        function()
+          require("neogen").generate({})
+        end,
+        desc = "Neogen Comment",
+      },
+    },
+    opts = { snippet_engine = "luasnip" },
+  },
+  {
+    "dinhhuy258/git.nvim",
+    event = "BufReadPre",
+    opts = {
+      keymaps = {
+        -- Open blame window
+        blame = "<Leader>gb",
+        -- Open file/folder in git repository
+        browse = "<Leader>go",
+      },
+    },
+  },
+  {
+    "folke/todo-comments.nvim",
+    opts = {
+      search = {
+        command = "rg",
+        args = {
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--glob=!node_modules",
+        },
+        pattern = [[\b(KEYWORDS):]],
+      },
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "L3MON4D3/LuaSnip",
+      "hrsh7th/cmp-emoji",
+    },
+    event = "VeryLazy",
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "emoji" })
+
+      vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+
+      local cmp = require("cmp")
+      local defaults = require("cmp.config.default")()
+
+      return {
+        completion = {
+          autocomplete = {
+            cmp.TriggerEvent.TextChanged,
+          },
+          completeopt = "menu,menuone,noinsert",
+        },
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-c>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<S-CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<C-CR>"] = function(fallback)
+            cmp.abort()
+            fallback()
+          end,
+        }),
+        sources = cmp.config.sources({
+          {
+            name = "luasnip",
+            priority = 150,
+            group_index = 1,
+            option = { show_autosnippets = false, use_show_condition = false },
+          },
+          { name = "nvim_lsp", priority = 100, group_index = 1 },
+          { name = "copilot", priority = 100, group_index = 2 },
+          { name = "buffer" },
+          { name = "path" },
+          { name = "emoji" },
+          { name = "dap" },
+        }, {
+          { name = "buffer" },
+        }),
+        formatting = {
+          format = function(_, item)
+            local icons = require("lazyvim.config").icons.kinds
+            if icons[item.kind] then
+              item.kind = icons[item.kind] .. item.kind
+            end
+            return item
+          end,
+        },
+        experimental = {
+          ghost_text = {
+            hl_group = "CmpGhostText",
+          },
+        },
+        sorting = defaults.sorting,
+      }
+    end,
+  },
+  -- {
+  --   "Exafunction/codeium.vim",
+  --   event = "BufEnter",
+  --   config = function()
+  --     -- Change '<C-g>' here to any keycode you like.
+  --     vim.keymap.set("i", "<C-g>", function()
+  --       return vim.fn["codeium#Accept"]()
+  --     end, { expr = true, silent = true })
+  --     vim.keymap.set("i", "<c-h>", function()
+  --       return vim.fn["codeium#CycleCompletions"](1)
+  --     end, { expr = true, silent = true })
+  --     vim.keymap.set("i", "<c-l>", function()
+  --       return vim.fn["codeium#CycleCompletions"](-1)
+  --     end, { expr = true, silent = true })
+  --     vim.keymap.set("i", "<c-x>", function()
+  --       return vim.fn["codeium#Clear"]()
+  --     end, { expr = true, silent = true })
+  --   end,
+  -- },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "VeryLazy",
+    config = function()
+      require("copilot").setup({
+        panel = {
+          enabled = true,
+          auto_refresh = false,
+          keymap = {
+            jump_prev = "[[",
+            jump_next = "]]",
+            accept = "<CR>",
+            refresh = "gr",
+            open = "<M-CR>",
+          },
+          layout = {
+            position = "bottom", -- | top | left | right
+            ratio = 0.4,
+          },
+        },
+        suggestion = {
+          enabled = false,
+          auto_trigger = true,
+          debounce = 75,
+          keymap = {
+            accept = "<M-l>",
+            accept_word = false,
+            accept_line = false,
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<C-]>",
+          },
+        },
+        filetypes = {
+          yaml = false,
+          markdown = false,
+          help = false,
+          gitcommit = false,
+          gitrebase = false,
+          hgcommit = false,
+          svn = false,
+          cvs = false,
+          ["."] = false,
+        },
+        copilot_node_command = "node", -- Node.js version must be > 18.x
+        server_opts_overrides = {},
+      })
+    end,
+  },
+  {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    opts = {},
+  },
+}
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -25,73 +25,66 @@ return {
         desc = "Find Plugin File",
       },
       {
-        ";p",
+        "<leader>fp",
         function()
           local builtin = require("telescope.builtin")
           builtin.find_files({
             no_ignore = false,
             hidden = true,
-            initial_mode = "normal",
           })
         end,
         desc = "Lists files in your current working directory, respects .gitignore",
       },
       {
-        ";f",
+        "<leader>ff",
         function()
           local builtin = require("telescope.builtin")
-          builtin.live_grep({
-            initial_mode = "normal",
-          })
+          builtin.live_grep()
         end,
         desc = "Search for a string in your current working directory and get results live as you type, respects .gitignore",
       },
       {
-        "\\\\",
+        "<leader>f\\",
         function()
           local builtin = require("telescope.builtin")
-          builtin.buffers({
-            initial_mode = "normal",
-          })
+          builtin.buffers()
         end,
         desc = "Lists open buffers",
       },
       {
-        ";t",
+        "<leader>ft",
         function()
           local builtin = require("telescope.builtin")
-          builtin.help_tags({
-            initial_mode = "normal",
-          })
+          builtin.help_tags()
         end,
         desc = "Lists available help tags and opens a new window with the relevant help info on <cr>",
       },
       {
-        ";;",
+        "<leader>f;",
         function()
           local builtin = require("telescope.builtin")
-          builtin.resume({ initial_mode = "normal" })
+          builtin.resume()
         end,
         desc = "Resume the previous telescope picker",
       },
       {
-        ";d",
+        "<leader>fd",
         function()
           local builtin = require("telescope.builtin")
-          builtin.diagnostics({ initial_mode = "normal" })
+          builtin.diagnostics()
         end,
         desc = "Lists Diagnostics for all open buffers or a specific buffer",
       },
       {
-        ";s",
+        "<leader>fs",
         function()
           local builtin = require("telescope.builtin")
-          builtin.treesitter({ initial_mode = "normal" })
+          builtin.treesitter()
         end,
         desc = "Lists Function names, variables, from Treesitter",
       },
       {
-        ";b",
+        "<leader>fb",
         function()
           local telescope = require("telescope")
 
@@ -106,8 +99,6 @@ return {
             hidden = true,
             grouped = true,
             previewer = true,
-            initial_mode = "normal",
-            -- layout_config = { height = 40 },
           })
         end,
         desc = "Open File Browser with the path of the current buffer",
@@ -129,6 +120,7 @@ return {
           "charting_library",
           "datafeeds",
         },
+        initial_mode = "normal",
         wrap_results = true,
         layout_strategy = "horizontal",
         layout_config = { prompt_position = "top" },
@@ -155,7 +147,6 @@ return {
       })
       opts.pickers = {
         diagnostics = {
-          initial_mode = "normal",
           layout_config = {
             preview_cutoff = 9999,
           },
@@ -167,7 +158,6 @@ return {
           -- disables netrw and use telescope-file-browser in its place
           hijack_netrw = true,
           previewer = true,
-          initial_mode = "normal",
           respect_gitignore = false,
           grouped = true,
           mappings = {
@@ -209,191 +199,7 @@ return {
   {
     { "LintaoAmons/cd-project.nvim" },
   },
-  {
-    "dinhhuy258/git.nvim",
-    event = "BufReadPre",
-    opts = {
-      keymaps = {
-        -- Open blame window
-        blame = "<Leader>gb",
-        -- Open file/folder in git repository
-        browse = "<Leader>go",
-        -- Opens a new diff that compares against the current index
-        diff = "<Leader>gd",
-        -- Close git diff
-        diff_close = "<Leader>gD",
-      },
-    },
-  },
-  {
-    "folke/todo-comments.nvim",
-    opts = {
-      search = {
-        command = "rg",
-        args = {
-          "--color=never",
-          "--no-heading",
-          "--with-filename",
-          "--line-number",
-          "--column",
-          "--glob=!node_modules",
-        },
-        pattern = [[\b(KEYWORDS):]],
-      },
-    },
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "L3MON4D3/LuaSnip",
-      "hrsh7th/cmp-emoji",
-    },
-    event = "VeryLazy",
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "emoji" })
-
-      vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
-
-      local cmp = require("cmp")
-      local defaults = require("cmp.config.default")()
-
-      return {
-        completion = {
-          autocomplete = {
-            cmp.TriggerEvent.TextChanged,
-          },
-          completeopt = "menu,menuone,noinsert",
-        },
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-c>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ["<S-CR>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ["<C-CR>"] = function(fallback)
-            cmp.abort()
-            fallback()
-          end,
-        }),
-        sources = cmp.config.sources({
-          { name = "copilot", keyword_length = 0 },
-          { name = "luasnip" },
-          { name = "nvim_lsp" },
-          { name = "buffer" },
-          { name = "path" },
-          { name = "dap" },
-        }, {
-          { name = "buffer" },
-        }),
-        formatting = {
-          format = function(_, item)
-            local icons = require("lazyvim.config").icons.kinds
-            if icons[item.kind] then
-              item.kind = icons[item.kind] .. item.kind
-            end
-            return item
-          end,
-        },
-        experimental = {
-          ghost_text = {
-            hl_group = "CmpGhostText",
-          },
-        },
-        sorting = defaults.sorting,
-      }
-    end,
-  },
-  -- {
-  --   "Exafunction/codeium.vim",
-  --   event = "BufEnter",
-  --   config = function()
-  --     -- Change '<C-g>' here to any keycode you like.
-  --     vim.keymap.set("i", "<C-g>", function()
-  --       return vim.fn["codeium#Accept"]()
-  --     end, { expr = true, silent = true })
-  --     vim.keymap.set("i", "<c-h>", function()
-  --       return vim.fn["codeium#CycleCompletions"](1)
-  --     end, { expr = true, silent = true })
-  --     vim.keymap.set("i", "<c-l>", function()
-  --       return vim.fn["codeium#CycleCompletions"](-1)
-  --     end, { expr = true, silent = true })
-  --     vim.keymap.set("i", "<c-x>", function()
-  --       return vim.fn["codeium#Clear"]()
-  --     end, { expr = true, silent = true })
-  --   end,
-  -- },
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    event = "VeryLazy",
-    config = function()
-      require("copilot").setup({
-        panel = {
-          enabled = true,
-          auto_refresh = false,
-          keymap = {
-            jump_prev = "[[",
-            jump_next = "]]",
-            accept = "<CR>",
-            refresh = "gr",
-            open = "<M-CR>",
-          },
-          layout = {
-            position = "bottom", -- | top | left | right
-            ratio = 0.4,
-          },
-        },
-        suggestion = {
-          enabled = false,
-          auto_trigger = true,
-          debounce = 75,
-          keymap = {
-            accept = "<M-l>",
-            accept_word = false,
-            accept_line = false,
-            next = "<M-]>",
-            prev = "<M-[>",
-            dismiss = "<C-]>",
-          },
-        },
-        filetypes = {
-          yaml = false,
-          markdown = false,
-          help = false,
-          gitcommit = false,
-          gitrebase = false,
-          hgcommit = false,
-          svn = false,
-          cvs = false,
-          ["."] = false,
-        },
-        copilot_node_command = "node", -- Node.js version must be > 18.x
-        server_opts_overrides = {},
-      })
-    end,
-  },
   -- lazy.nvim
-  {
-    "m4xshen/hardtime.nvim",
-    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-    opts = {},
-  },
   {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
