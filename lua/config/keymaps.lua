6a31a66b91ff4f591e1347b48a36455ab2696d58
Davgatseren <25279427+z3tsubouou@users.noreply.github.com>
2024-07-29 16:07:49 +0800

    keymap fix, git plugin keymap changed

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -24,10 +24,10 @@ keymap.set("n", "<leader>tn", "<Cmd>term<CR>", { desc = "New terminal tab" })
 -- keymap.set('n', 'tb', 'gT')
 
 -- Split window
-keymap.set("n", "<leader>wx", "<cmd>split<CR>", { desc = "Split window below" })
-keymap.set("n", "<leader>wv", "<cmd>vsplit<CR>", { desc = "Split window right" })
-keymap.set("n", "<leader>wn", "<cmd>cnext<CR>", { desc = "Quicklist next" })
-keymap.set("n", "<leader>wp", "<cmd>cprev<CR>", { desc = "Quicklist prev" })
+keymap.set("", "<leader>wx", "<cmd>split<CR>", { desc = "Split window below" })
+keymap.set("", "<leader>wv", "<cmd>vsplit<CR>", { desc = "Split window right" })
+keymap.set("", "<leader>wn", "<cmd>cnext<CR>", { desc = "Quicklist next" })
+keymap.set("", "<leader>wp", "<cmd>cprev<CR>", { desc = "Quicklist prev" })
 
 -- Move window
 -- keymap.set('n', '<leader>', '<C-w>w')
--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -20,8 +20,26 @@ return {
       keymaps = {
         -- Open blame window
         blame = "<Leader>gb",
+        -- Close blame window
+        quit_blame = "q",
+        -- Open blame commit
+        blame_commit = "<CR>",
+        -- Quit blame commit
+        quit_blame_commit = "q",
         -- Open file/folder in git repository
         browse = "<Leader>go",
+        -- Open pull request of the current branch
+        open_pull_request = "",
+        -- Create a pull request with the target branch is set in the `target_branch` option
+        create_pull_request = "",
+        -- Opens a new diff that compares against the current index
+        diff = "<Leader>gd",
+        -- Close git diff
+        diff_close = "q",
+        -- Revert to the specific commit
+        revert = "<Leader>gr",
+        -- Revert the current file to the specific commit
+        revert_file = "<Leader>gR",
       },
     },
   },
