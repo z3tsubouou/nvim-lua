42ce0c1346b8ae6c13cea364344133685997879d
Davgatseren <25279427+z3tsubouou@users.noreply.github.com>
2024-08-14 16:14:44 +0800

    removed comment

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -35,7 +35,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.util.dot" },
 
-    -- { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.dap.core" },
 
     { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
