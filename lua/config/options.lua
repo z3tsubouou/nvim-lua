b39af01081bd9d930fc230fcc8b101074417dc7a
Davgatseren <25279427+z3tsubouou@users.noreply.github.com>
2024-08-14 15:03:33 +0800

    lsp zaswar

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -40,6 +40,9 @@ vim.opt.splitright = true -- Put new windows right of current
 vim.opt.splitkeep = "cursor"
 vim.opt.mouse = ""
 
+--language
+vim.cmd("language en_US.utf-8")
+
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -28,6 +28,57 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+        vtsls = {
+          enabled = false,
+        },
+        tsserver = {
+          enabled = true,
+          settings = {
+            typescript = {
+              inlayHints = {
+                includeInlayEnumMemberValueHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayParameterNameHints = "literals",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayVariableTypeHints = false,
+                includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+              },
+            },
+            completions = {
+              completeFunctionCalls = true,
+            },
+          },
+          keys = {
+            {
+              "<leader>co",
+              function()
+                vim.lsp.buf.code_action({
+                  apply = true,
+                  context = {
+                    only = { "source.organizeImports.ts" },
+                    diagnostics = {},
+                  },
+                })
+              end,
+              desc = "Organize Imports",
+            },
+            {
+              "<leader>cu",
+              function()
+                vim.lsp.buf.code_action({
+                  apply = true,
+                  context = {
+                    only = { "source.removeUnused.ts" },
+                    diagnostics = {},
+                  },
+                })
+              end,
+              desc = "Remove Unused Imports",
+            },
+          },
+        },
         yamlls = {
           settings = {
             yaml = {
@@ -64,6 +115,14 @@ return {
             },
           },
         },
+      },
+      setup = {
+        tsserver = function()
+          return false
+        end,
+        vtsls = function()
+          return true
+        end,
       },
     },
   },
