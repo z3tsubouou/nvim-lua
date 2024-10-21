47f19ddf0cd7f12ae7860c71f0dd7a9f9ed70188
Davgatseren <25279427+z3tsubouou@users.noreply.github.com>
2024-10-21 16:47:50 +0800

    order

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,17 +13,20 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
 
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.tailwind" },
-    { import = "lazyvim.plugins.extras.lang.docker" },
-    { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
-    { import = "lazyvim.plugins.extras.lang.toml" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
+
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.svelte" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.php" },
     -- { import = "lazyvim.plugins.extras.lang.java" },
 
     { import = "lazyvim.plugins.extras.formatting.black" },
