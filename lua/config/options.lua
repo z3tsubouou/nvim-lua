73f7728526eb84a166c73d7c56d23f147cb43eb1
Davgatseren <davgatseren@gmail.com>
2024-11-13 16:23:46 +0800

    php update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,7 +7,7 @@ vim.g.lazyvim_prettier_needs_config = false
 vim.g.mapleader = " "
 vim.g.deprecation_warnings = true
 vim.g.lazyvim_python_lsp = "basedpyright"
-vim.g.lazyvim_php_lsp = "phpactor"
+vim.g.lazyvim_php_lsp = "intelephense"
 -- In case you don't want to use `:LazyExtras`,
 -- then you need to set the option below.
 vim.g.lazyvim_picker = "fzf"
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -33,6 +33,7 @@ return {
         "gosum",
         "svelte",
         "php",
+        "blade",
       },
       incremental_selection = {
         enable = true,
@@ -53,5 +54,23 @@ return {
         },
       },
     },
+    config = function(_, opts)
+      vim.filetype.add({
+        pattern = {
+          [".*%.blade%.php"] = "blade",
+        },
+      })
+
+      require("nvim-treesitter.configs").setup(opts)
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.blade = {
+        install_info = {
+          url = "https://github.com/EmranMR/tree-sitter-blade",
+          files = { "src/parser.c" },
+          branch = "main",
+        },
+        filetype = "blade",
+      }
+    end,
   },
 }
