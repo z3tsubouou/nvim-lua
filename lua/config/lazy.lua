b1729c0d2235058062ccd5d3e8edf523a0aca854
Davgatseren <davgatseren@gmail.com>
2024-12-05 10:10:27 +0800

    update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -48,6 +48,7 @@ require("lazy").setup({
 
     { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
+    { import = "lazyvim.plugins.extras.ui.dashboard-nvim" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,6 +11,7 @@ vim.g.lazyvim_php_lsp = "intelephense"
 -- In case you don't want to use `:LazyExtras`,
 -- then you need to set the option below.
 vim.g.lazyvim_picker = "fzf"
+-- vim.g.lazyvim_picker = "telescope"
 
 vim.scriptencoding = "utf-8"
 vim.opt.encoding = "utf-8"
